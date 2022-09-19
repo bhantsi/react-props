b751bb7fabe20df287be84c706fb78939d6abbdc
bhantsi <bhantsi@gmail.com>
2022-09-19 15:01:36 +0100

    fix typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Header() {
     return (
       <div className="lead--txt">
-        <h1>React Props Example </h1>
+        <h1>React Props Working Example </h1>
         <p>
           This is a simple react props example, you can see the code
-          <a href="https://github.com/bhantsi/react-props">on github</a>
+          <a href="https://github.com/bhantsi/react-props"> on github</a>
         </p>
         <br />
       </div>
