bc705f982813c774542227e167cc1b4597fd965b
bhantsi <bhantsi@gmail.com>
2022-09-19 14:55:25 +0100

    Fix grammar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function Header() {
     return (
-        <div className="lead--txt">
-             <h1>React Props Example </h1>
-      <p>
-        This is a simple react props example, you can see the  <a href="https://github.com/bhantsi/react-props">code here</a> 
-      </p>
-      <br />
-        </div>
-    )
+      <div className="lead--txt">
+        <h1>React Props Example </h1>
+        <p>
+          This is a simple react props example, you can see the code
+          <a href="https://github.com/bhantsi/react-props">on github</a>
+        </p>
+        <br />
+      </div>
+    );
 }
